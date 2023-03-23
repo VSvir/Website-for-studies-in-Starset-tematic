a3a0ecd6b11e30e4da5c3247d229d0b7799e2c66
VSvir <VSslava612@yandex.ru>
2023-03-23 18:50:38 +0300

    refactor(settings) security issues

--- djangoStarsetWebsiteProject/settings.py
+++ djangoStarsetWebsiteProject/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from decouple import config
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -20,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r^2puhge)9+^f4t=)10cni6)_5polvni4%l1um%u99@5en=713'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
